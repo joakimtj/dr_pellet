239adce942145df3d3a3c69baa2f5ea203c5faa8
yuna04 <joakimtj@hiof.no>
2024-01-06 09:40:57 +0100

    init_grid now properly initializes. bg render and grid area

--- dr_pellet/main.c
+++ dr_pellet/main.c
@@ -12,8 +12,8 @@
 #define WINDOW_HEIGHT 800 
 #define GRID_ROWS 12 // The height of the play-area.
 #define GRID_COLUMNS 8 // The width of the play-area.
-#define GRID_ROW_CLEARANCE 6 // Viruses will not spawn above or on this ROW.
-#define ENTITY_N 96 // The N viruses to be spawned on the grid.
+#define GRID_ROW_CLEARANCE 4 // Viruses will not spawn above or on this ROW.
+#define ENTITY_N 66 // The N viruses to be spawned on the grid.
 #define RECT_SIZE 30
 #define PADDING 1
 
@@ -39,7 +39,6 @@ struct {
 } typedef entity;
 
 struct {
-	bool empty;
 	entity* c_entity;
 } typedef cell;
 
@@ -51,11 +50,15 @@ struct {
 	bool has_moved;
 } typedef pill;
 
+int get_random_integer(int i) {
+	return rand() % i;
+}
+
 SDL_Rect* get_rect(cell c) {
 	return &c.c_entity->rect;
 }
 
-void set_position_rect(SDL_Rect* rect, int i, int j) {
+void set_grid_position_rect(SDL_Rect* rect, int i, int j) {
 	rect->x = (WINDOW_WIDTH / 2) - (GRID_ROWS * RECT_SIZE) + (PADDING + RECT_SIZE) * j;
 	rect->y = (WINDOW_HEIGHT / 4) + (PADDING + RECT_SIZE) * i;
 	rect->w = RECT_SIZE;
@@ -64,17 +67,34 @@ void set_position_rect(SDL_Rect* rect, int i, int j) {
 
 void init_grid(cell grid[GRID_ROWS][GRID_COLUMNS], entity* entities) {
 	int entity_count = 0;
+	int random_i;
 	for (int i = 0; i < GRID_ROWS; i++)
 	{
 		for (int j = 0; j < GRID_COLUMNS; j++)
-		{
-			if (entity_count < ENTITY_N) 
+		{	
+			if (i > GRID_ROW_CLEARANCE) 
 			{
-					grid[i][j].empty = false;
-					grid[i][j].c_entity = &entities[entity_count];
-					set_position_rect(&grid[i][j].c_entity->rect, i, j);
-					entity_count++;
+				if (entity_count < ENTITY_N)
+				{
+					random_i = get_random_integer(10);
+					if (random_i == 0) {
+						grid[i][j].c_entity = NULL;
+					}
+
+					else {
+						grid[i][j].c_entity = &entities[entity_count];
+						set_grid_position_rect(&grid[i][j].c_entity->rect, i, j);
+						entity_count++;
+					}
+				}
+				else {
+					grid[i][j].c_entity = NULL;
+				}
 			}
+			else {
+				grid[i][j].c_entity = NULL;
+			}
+			
 		}
 	}
 	printf("entity count: %d\n", entity_count);
@@ -112,7 +132,6 @@ entity create_entity(entity_type e_type, SDL_Rect rect) {
 	return;
 }
 entity* init_entities() {
-	#define ENTITY_N 96 
 	int RED_COUNT = 0, BLUE_COUNT = 0, YELLOW_COUNT = 0;
 	entity* entities = malloc(sizeof(entity) * ENTITY_N);
 	entity_type e_type;
@@ -148,6 +167,52 @@ void move_pill(cell* old_c, cell* new_c, int i) {
 	old_c->c_entity = tmp.c_entity;
 }
 
+void render_grid_area(SDL_Renderer* renderer) {
+	SDL_Rect bg_rect;
+	bg_rect.x = WINDOW_WIDTH / 5;
+	bg_rect.y = WINDOW_HEIGHT / 4;
+	bg_rect.w = GRID_COLUMNS * RECT_SIZE + PADDING + 6;
+	bg_rect.h = GRID_ROWS * RECT_SIZE + PADDING + 10;
+
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+	SDL_SetRenderDrawColor(renderer, 0xd5, 0xab, 0xff, 200);
+	SDL_RenderFillRect(renderer, &bg_rect);
+	SDL_RenderDrawRect(renderer, &bg_rect);
+
+	SDL_SetRenderDrawColor(renderer, 25, 25, 25, 255);
+}
+
+void render_bg(SDL_Renderer* renderer) {
+	for (int i = 0; i <= WINDOW_HEIGHT / RECT_SIZE; i++)
+	{
+		for (int j = 0; j <= WINDOW_WIDTH / RECT_SIZE; j++)
+		{
+			SDL_Rect bg_rect = create_rect();
+			bg_rect.x = -16 + RECT_SIZE * j;
+			bg_rect.y = -2 + RECT_SIZE * i;
+			bg_rect.w = RECT_SIZE;
+			bg_rect.h = RECT_SIZE;
+
+			if (j % 2 == 0) {
+
+				SDL_SetRenderDrawColor(renderer, 0x8b, 0xb4, 0xd9, 255);
+				SDL_RenderFillRect(renderer, &bg_rect);
+				SDL_RenderDrawRect(renderer, &bg_rect);
+			}
+			else if (i % 2 == 0) {
+				SDL_SetRenderDrawColor(renderer, 0x8b, 0xb4, 0xd9, 255);
+				SDL_RenderFillRect(renderer, &bg_rect);
+				SDL_RenderDrawRect(renderer, &bg_rect);
+			}
+			else {
+				SDL_SetRenderDrawColor(renderer, 0x8b, 0xd9, 0xd2, 255);
+				SDL_RenderFillRect(renderer, &bg_rect);
+				SDL_RenderDrawRect(renderer, &bg_rect);
+			}
+		}
+	}
+}
+
 int main(int argc, char** argv)
 {
 	SDL_Init(SDL_INIT_VIDEO);
@@ -160,7 +225,7 @@ int main(int argc, char** argv)
 	entity* entities = init_entities();
 	cell grid[GRID_ROWS][GRID_COLUMNS];
 	init_grid(grid, entities);
-	
+
 	SDL_Surface* red_block_bmp = SDL_LoadBMP("red_block.bmp");
 	SDL_Texture* red_block_t = SDL_CreateTextureFromSurface(renderer, red_block_bmp);
 	SDL_FreeSurface(red_block_bmp);
@@ -203,15 +268,17 @@ int main(int argc, char** argv)
 
 		// Rendering
 		SDL_RenderClear(renderer);
-		SDL_RenderCopy(renderer, bg_t, NULL, NULL);
-		SDL_SetRenderDrawColor(renderer, 25, 25, 25, 255);
+		
+		render_bg(renderer);
+
+		render_grid_area(renderer);
 
 		for (int i = 0; i < GRID_ROWS; i++) 
 		{
 			for (int j = 0; j < GRID_COLUMNS; j++) 
 			{
-				if (!grid[i][j].empty) 
-				{
+				if (grid[i][j].c_entity != NULL) 
+				{	
 					if (grid[i][j].c_entity->type == RED_VIRUS) {
 						SDL_RenderCopy(renderer, red_block_t, NULL, get_rect(grid[i][j]));
 					}
@@ -230,18 +297,6 @@ int main(int argc, char** argv)
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderPresent(renderer);
 	}
-	int count = 0;
-	for (int i = 0; i < GRID_ROWS; i++) {
-		for (int j = 0; j < GRID_COLUMNS; j++) {
-			if (grid[i][j].c_entity != NULL) count++;
-		}
-	}
-	printf("Populated cells: %d\n", count);
-	count = 0;
-	for (int i = 0; i < ENTITY_N; i++) {
-		printf("i: %d, type: %d \n", i, entities[i].type);
-		count++;
-	}
-	printf("Entities in entities list: %d\n", count);
+
 	return 0;
 }
